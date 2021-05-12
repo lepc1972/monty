5328a132abaef8dd4266820823b2ad93c8163557
lepc1972 <githublepc@gmail.com>
2021-05-12 22:36:18 +0000

    montyc fixes

---
+++ operation.c
@@ -0,0 +1,89 @@
+#include "monty.h"
+
+/**
+ * o_push     - Push a node on top of stack top
+ *
+ * @stack:       doubly linked list
+ * @line_number: Current line number in monty file
+ */
+
+void o_push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new;
+	unsigned int size;
+	int number;
+
+	if (global.value == NULL)
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	size = atoi(global.value) <= 0 ? 1 : 0;
+	number = abs(atoi(global.value));
+	while (number > 0)
+	{
+		number /= 10;
+		size++;
+	}
+	if (size != strlen(global.value))
+	{
+		printf("L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new->n = atoi(global.value);
+	new->prev = NULL;
+	new->next = *stack;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new;
+	}
+	*stack = new;
+	global.head = new;
+}
+
+/**
+ * o_pop       - Removes the top element of @stack
+ *
+ * @stack:        A doubly linked list
+ * @line_number:  Current line number in monty script file
+ */
+
+void o_pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *target;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("L%u: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	target = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+	{
+		(*stack)->prev = NULL;
+	}
+
+	free(target);
+}
+
+/**
+ * o_nop       - Does nothing
+ *
+ * @stack:        A doubly linked list
+ * @line_number:  Current line number in monty script file
+ */
+
+void o_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
