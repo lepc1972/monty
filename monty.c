f4a855ce347c0c9d01cc8174fc92b794516ee4ca
lepc1972 <githublepc@gmail.com>
2021-05-12 22:37:13 +0000

    more fixes

--- monty.c
+++ monty.c
@@ -3,105 +3,105 @@
 global_t global;
 
 /**
- * free_stack      - Free the memory
- */
+* free_stack      - Free the memory
+*/
 
 void free_stack(void)
 {
-	if (global.head != NULL)
-	{
-		while (global.head->next != NULL)
-		{
-			global.head = global.head->next;
-			free(global.head->prev);
-		}
-		free(global.head);
-	}
+if (global.head != NULL)
+{
+while (global.head->next != NULL)
+{
+global.head = global.head->next;
+free(global.head->prev);
+}
+free(global.head);
+}
 }
 
 
 /**
- * find_function   - Find function to match argument
- *
- * @stack:           Head of stack data structure
- * @line_number:     current line in monty file
- */
+* find_function   - Find function to match argument
+*
+* @stack:           Head of stack data structure
+* @line_number:     current line in monty file
+*/
 
 void find_function(stack_t **stack, unsigned int line_number)
 {
-	unsigned int i = 0;
-	instruction_t functions[] = {
-		{"push", o_push}, {"pall", o_pall},
-		{"pint", o_pint}, {"pop", o_pop},
-		{"swap", o_swap}, {"add", o_add},
-		{"nop", o_nop}, {NULL, NULL}
-	};
+unsigned int i = 0;
+instruction_t functions[] = {
+{"push", o_push}, {"pall", o_pall},
+{"pint", o_pint}, {"pop", o_pop},
+{"swap", o_swap}, {"add", o_add},
+{"nop", o_nop}, {NULL, NULL}
+};
 
-	while (functions[i].opcode != NULL)
-	{
-		if (strcmp(global.op_name, functions[i].opcode) == 0)
-		{
-			functions[i].f(stack, line_number);
-			break;
-		}
-		i++;
-	}
+while (functions[i].opcode != NULL)
+{
+if (strcmp(global.op_name, functions[i].opcode) == 0)
+{
+functions[i].f(stack, line_number);
+break;
+}
+i++;
+}
 
-	/* Could not find command in array, invalid command */
-	if (functions[i].opcode == NULL)
-	{
-		printf("L%u: unknown instruction %s\n", line_number, global.op_name);
-		exit(EXIT_FAILURE);
-	}
+/* Could not find command in array, invalid command */
+if (functions[i].opcode == NULL)
+{
+printf("L%u: unknown instruction %s\n", line_number, global.op_name);
+exit(EXIT_FAILURE);
+}
 }
 
 
 /**
- * main     - Interprets Monty bytecodes
- *
- * @argc:     Number of arguments
- * @argv:     Array of arguments
- *
- * Return:    Success: EXIT_SUCCESS
- *            Failure: EXIT_FAILURE
- */
+* main     - Interprets Monty bytecodes
+*
+* @argc:     Number of arguments
+* @argv:     Array of arguments
+*
+* Return:    Success: EXIT_SUCCESS
+*            Failure: EXIT_FAILURE
+*/
 
 int main(int argc, char **argv)
 {
-	stack_t *head = NULL;
-	FILE *fp;
-	char *line = NULL;
-	size_t line_length = 0;
-	unsigned int line_number = 1;
+stack_t *head = NULL;
+FILE *fp;
+char *line = NULL;
+size_t line_length = 0;
+unsigned int line_number = 1;
 
-	if (argc < 2)
-	{
-		printf("USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
+if (argc < 2)
+{
+printf("USAGE: monty file\n");
+exit(EXIT_FAILURE);
+}
 
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
-	{
-		printf("Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	atexit(free_stack);
+fp = fopen(argv[1], "r");
+if (fp == NULL)
+{
+printf("Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
+}
+atexit(free_stack);
 
-	while (getline(&line, &line_length, fp) != -1)
-	{
-		global.op_name = strtok(line, "\n\t\r\v\f ");
-		global.value = strtok(NULL, "\n\t\r\v\f ");
+while (getline(&line, &line_length, fp) != -1)
+{
+global.op_name = strtok(line, "\n\t\r\v\f ");
+global.value = strtok(NULL, "\n\t\r\v\f ");
 
-		if (global.op_name != NULL)
-		{
-			find_function(&head, line_number);
-		}
-		line_number++;
-	}
+if (global.op_name != NULL)
+{
+find_function(&head, line_number);
+}
+line_number++;
+}
 
-	free(line);
-	fclose(fp);
+free(line);
+fclose(fp);
 
-	exit(EXIT_SUCCESS);
+exit(EXIT_SUCCESS);
 }
