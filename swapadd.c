455df4a6c6290e6d3de93a28a9d44c3d28fcc18d
lepc1972 <githublepc@gmail.com>
2021-05-13 02:02:53 +0000

    swapadd fix

---
+++ swapadd.c
@@ -0,0 +1,45 @@
+#include "monty.h"
+
+/**
+ * o_swap      - Swaps top two elements of stack
+ *
+ * @stack:        A doubly linked list
+ * @line_number:  line number in monty file
+ */
+
+void o_swap(stack_t **stack, unsigned int line_number)
+{
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->n = (*stack)->n + (*stack)->next->n;
+	(*stack)->next->n = (*stack)->n - (*stack)->next->n;
+	(*stack)->n = (*stack)->n - (*stack)->next->n;
+}
+
+/**
+ * o_add       - Adds the top two elements of @stack
+ *
+ * @stack:        A doubly linked list
+ * @line_number:  line number in monty file
+ */
+
+void o_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *target;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	target = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+
+	free(target);
+}
